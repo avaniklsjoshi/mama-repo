a1f09da4b7e8943376797615f7cdada36cbf1135
Avani Joshi <avaniklsjoshi@gmail.com>
2022-08-30 11:13:00 +0200

    message updated

--- apps/frontend-joshi/src/Components/Views/LandingPage/Introduction/introduction.tsx
+++ apps/frontend-joshi/src/Components/Views/LandingPage/Introduction/introduction.tsx
@@ -5,7 +5,9 @@ export default function Introduction() {
   return (
     <div className="intro-container">
       <div className="intro-left">
-        <div className="intro-title">Hi, I am Avani. A Frontend Enthusiast</div>
+        <div className="intro-title">
+          Hi, I am Avani. A Frontend Enthusiast.
+        </div>
         <div className="intro-desc">
           I am an experienced Javascript Developer and I love to make
           interactive end-to-end projects.
