c21c1bde4e2bd19f23be03c65f8472f4ee0a6e50
Avani Joshi <avaniklsjoshi@gmail.com>
2022-08-30 10:31:43 +0200

    frontend-joshi: intro  added

--- apps/frontend-joshi/src/Components/Views/LandingPage/Introduction/introduction.tsx
+++ apps/frontend-joshi/src/Components/Views/LandingPage/Introduction/introduction.tsx
@@ -1,16 +1,19 @@
 import "./landingPage.scss";
+import SubMarine from "./subMarine";
 
 export default function Introduction() {
   return (
     <div className="intro-container">
       <div className="intro-left">
-        <div className="intro-title">Hi, I am Avani. A developer</div>
+        <div className="intro-title">Hi, I am Avani. A Frontend Enthusiast</div>
         <div className="intro-desc">
           I am an experienced Javascript Developer and I love to make
           interactive end-to-end projects.
         </div>
       </div>
-      <div className="intro-right"></div>
+      <div className="intro-right">
+        <SubMarine />
+      </div>
     </div>
   );
 }
