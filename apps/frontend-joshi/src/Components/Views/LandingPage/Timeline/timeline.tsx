eeb5a84af585cba365908968f4620d3e1471ec04
Avani Joshi <avaniklsjoshi@gmail.com>
2022-09-06 11:40:34 +0200

    frontend-joshi: timeline section data separated

--- apps/frontend-joshi/src/Components/Views/LandingPage/Timeline/timeline.tsx
+++ apps/frontend-joshi/src/Components/Views/LandingPage/Timeline/timeline.tsx
@@ -1,46 +1,67 @@
+import { useEffect, useState } from "react";
 import "./timeline.scss";
 import { IconButton, Tooltip } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import InfoButton from "./infoButton";
 import { InfoIcon } from "../../../../Assets/SVG/infoIcon";
-import { EXPERIENCE_TIMELINE } from "../../../../Configs/Constants/const";
+import { LANGUAGE_NAMES_KEY } from "../../../../Configs/Constants/const";
+import {
+  EXPERIENCE_TIMELINE_de,
+  EXPERIENCE_TIMELINE_en,
+  EXPERIENCE_TIMELINE_hn
+} from "../../../../Configs/Constants/constWithTranslation";
 
 export default function Timeline() {
+  const { i18n } = useTranslation();
+  const [activeTimeline, setActiveTimeline] = useState(EXPERIENCE_TIMELINE_en);
+
+  useEffect(() => {
+    if (i18n.language == LANGUAGE_NAMES_KEY.ENGLISH) {
+      setActiveTimeline(() => EXPERIENCE_TIMELINE_en);
+    } else if (i18n.language == LANGUAGE_NAMES_KEY.GERMAN) {
+      setActiveTimeline(() => EXPERIENCE_TIMELINE_de);
+    } else {
+      setActiveTimeline(() => EXPERIENCE_TIMELINE_hn);
+    }
+  }, [i18n.language]);
+
   return (
     <div className="timeline">
-      <div className="timeline-title">HOW IT ALL STARTED</div>
+      <div className="timeline-title">HOW IT ALL STARTED {i18n.language}</div>
 
-      {EXPERIENCE_TIMELINE.map((exp) => (
-        <div
-          className="timeline__event  animated fadeInUp delay-3s timeline__event--type1"
-          key={exp.title}
-        >
-          <div className="timeline__event__icon ">
-            <i className="lni-cake">
-              <img src={exp.thumbnail} alt="jiji" />
-            </i>
-          </div>
-          <div className="timeline__event__date">{exp.span}</div>
-          <div className="timeline__event__content ">
-            <div className="timeline__event__title">
-              {exp.title}
-              <Tooltip
-                title={
-                  <InfoButton content={exp.info} projects={exp.projects} />
-                }
-                className="info-icon"
-              >
-                <IconButton>
-                  <InfoIcon className="feature-iicon" />
-                </IconButton>
-              </Tooltip>
+      {activeTimeline &&
+        activeTimeline.map((exp: any) => (
+          <div
+            className="timeline__event  animated fadeInUp delay-3s timeline__event--type1"
+            key={exp.title}
+          >
+            <div className="timeline__event__icon ">
+              <i className="lni-cake">
+                <img src={exp.thumbnail} alt="jiji" />
+              </i>
             </div>
-            <div className="timeline__event__description">
-              <p>Projects- {exp.projects}</p>
-              <p>Role- {exp.role}</p>
+            <div className="timeline__event__date">{exp.span}</div>
+            <div className="timeline__event__content ">
+              <div className="timeline__event__title">
+                {exp.title}
+                <Tooltip
+                  title={
+                    <InfoButton content={exp.info} projects={exp.projects} />
+                  }
+                  className="info-icon"
+                >
+                  <IconButton>
+                    <InfoIcon className="feature-iicon" />
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <div className="timeline__event__description">
+                <p>Projects- {exp.projects}</p>
+                <p>Role- {exp.role}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
---
+++ apps/frontend-joshi/src/Configs/Constants/constWithTranslation.ts
@@ -0,0 +1,223 @@
+export const EXPERIENCE_TIMELINE_en = [
+  {
+    title: "Etecture Gmbh, Software Developer",
+    span: "12-09-2019 - Untill",
+    role: "Designing Architecture, Basic Setting up of Apps, Implementation",
+    projects: "VW AutoSuche, VW Onehub, ING db GeldAutomat",
+    info: {
+      tech: [
+        "React, Redux, HTML, CSS, Typescript, Jest testing framework, storybook, styled components, Webpack, TSLint",
+        "Volkswagen’s components",
+        "node js"
+      ],
+      role: [
+        "Cross team communication with Volkswagen, FSAG, Dayone, FME, and Ukraine.",
+        "Making proposals and implementation.",
+        "Taking ownership and responsibility for the project and the code.",
+        "Team mentoring",
+        "Improved performance of animations with the ‘system GPU Animation’"
+      ]
+    },
+    thumbnail: "../../../assets/Images/sauropod-svgrepo-com.svg"
+  },
+  {
+    title: "Ticketmaster, Frontend Developer",
+    span: "09/2017 – 05/2019",
+    role: "Creating device and browser compatible apps.",
+    projects: "CMS, Virtual Venue",
+    info: {
+      tech: [
+        "React, Redux, AppSync, HTML, CSS, SCSS, Webpack, ESLint",
+        "Froala editor, sagas, mapbox, krPano",
+        "Node JS, Lambda, DynamoDB, Terra Formars, pipelining, cloud watch",
+        "Creating an SVG images dynamically."
+      ],
+      role: [
+        "Implementated several modules at backend with AWS architecture.",
+        "Team mentoring",
+        "Taking ownership and responsibility for the project and the code.",
+        "Involved in requirement gathering and analysis and deciding implementation."
+      ]
+    },
+    thumbnail: "../../../Assets/Images/flower-svgrepo-com.svg"
+  },
+  {
+    title: "WebSenor Technologies, Fullstack Developer",
+    span: "02/15 – 07/17",
+    role: "requirement gathering, designing and Implementation.",
+    projects: "Nova Farma",
+    info: {
+      tech: ["PHP", "MySQL, HTML, CSS, Javascript"],
+      role: [
+        "Routing mechanism",
+        "Communicating with stakeholders, requirement gathering, and designing.",
+        "Implemented complex medicine warehousing logic"
+      ]
+    },
+    thumbnail: "../../Assets/Images/dinosaur-svgrepo-com.svg"
+  },
+  {
+    title: "Infosys Ltd, Trainee",
+    span: "09/14 – 12/14",
+    role: "Learning Microsoft .NET Technologies",
+    projects: "Shopping portal",
+    info: {
+      tech: ["ASP.NET, ADO.NET", "Java Script, AJAX, HTML, CSS", "MS SQL"],
+      role: [
+        "It is a shopping website using which we can manage admin, user accounts.",
+        "Add/remove products.",
+        "buy products through various WCF and WEB Services, stored procedures, functions."
+      ]
+    },
+    thumbnail: "../../Assets/Images/flower-green.svg"
+  }
+];
+
+export const EXPERIENCE_TIMELINE_hn = [
+  {
+    title: "Etecture Gmbh, Software Developer hn",
+    span: "12-09-2019 - Untill",
+    role: "Designing Architecture, Basic Setting up of Apps, Implementation",
+    projects: "VW AutoSuche, VW Onehub, ING db GeldAutomat",
+    info: {
+      tech: [
+        "React, Redux, HTML, CSS, Typescript, Jest testing framework, storybook, styled components, Webpack, TSLint",
+        "Volkswagen’s components",
+        "node js"
+      ],
+      role: [
+        "Cross team communication with Volkswagen, FSAG, Dayone, FME, and Ukraine.",
+        "Making proposals and implementation.",
+        "Taking ownership and responsibility for the project and the code.",
+        "Team mentoring",
+        "Improved performance of animations with the ‘system GPU Animation’"
+      ]
+    },
+    thumbnail: "../../../assets/Images/sauropod-svgrepo-com.svg"
+  },
+  {
+    title: "Ticketmaster, Frontend Developer",
+    span: "09/2017 – 05/2019",
+    role: "Creating device and browser compatible apps.",
+    projects: "CMS, Virtual Venue",
+    info: {
+      tech: [
+        "React, Redux, AppSync, HTML, CSS, SCSS, Webpack, ESLint",
+        "Froala editor, sagas, mapbox, krPano",
+        "Node JS, Lambda, DynamoDB, Terra Formars, pipelining, cloud watch",
+        "Creating an SVG images dynamically."
+      ],
+      role: [
+        "Implementated several modules at backend with AWS architecture.",
+        "Team mentoring",
+        "Taking ownership and responsibility for the project and the code.",
+        "Involved in requirement gathering and analysis and deciding implementation."
+      ]
+    },
+    thumbnail: "../../../Assets/Images/flower-svgrepo-com.svg"
+  },
+  {
+    title: "WebSenor Technologies, Fullstack Developer",
+    span: "02/15 – 07/17",
+    role: "requirement gathering, designing and Implementation.",
+    projects: "Nova Farma",
+    info: {
+      tech: ["PHP", "MySQL, HTML, CSS, Javascript"],
+      role: [
+        "Routing mechanism",
+        "Communicating with stakeholders, requirement gathering, and designing.",
+        "Implemented complex medicine warehousing logic"
+      ]
+    },
+    thumbnail: "../../Assets/Images/dinosaur-svgrepo-com.svg"
+  },
+  {
+    title: "Infosys Ltd, Trainee",
+    span: "09/14 – 12/14",
+    role: "Learning Microsoft .NET Technologies",
+    projects: "Shopping portal",
+    info: {
+      tech: ["ASP.NET, ADO.NET", "Java Script, AJAX, HTML, CSS", "MS SQL"],
+      role: [
+        "It is a shopping website using which we can manage admin, user accounts.",
+        "Add/remove products.",
+        "buy products through various WCF and WEB Services, stored procedures, functions."
+      ]
+    },
+    thumbnail: "../../Assets/Images/flower-green.svg"
+  }
+];
+export const EXPERIENCE_TIMELINE_de = [
+  {
+    title: "Etecture Gmbh, Software Developer de",
+    span: "12-09-2019 - Untill",
+    role: "Designing Architecture, Basic Setting up of Apps, Implementation",
+    projects: "VW AutoSuche, VW Onehub, ING db GeldAutomat",
+    info: {
+      tech: [
+        "React, Redux, HTML, CSS, Typescript, Jest testing framework, storybook, styled components, Webpack, TSLint",
+        "Volkswagen’s components",
+        "node js"
+      ],
+      role: [
+        "Cross team communication with Volkswagen, FSAG, Dayone, FME, and Ukraine.",
+        "Making proposals and implementation.",
+        "Taking ownership and responsibility for the project and the code.",
+        "Team mentoring",
+        "Improved performance of animations with the ‘system GPU Animation’"
+      ]
+    },
+    thumbnail: "../../../assets/Images/sauropod-svgrepo-com.svg"
+  },
+  {
+    title: "Ticketmaster, Frontend Developer",
+    span: "09/2017 – 05/2019",
+    role: "Creating device and browser compatible apps.",
+    projects: "CMS, Virtual Venue",
+    info: {
+      tech: [
+        "React, Redux, AppSync, HTML, CSS, SCSS, Webpack, ESLint",
+        "Froala editor, sagas, mapbox, krPano",
+        "Node JS, Lambda, DynamoDB, Terra Formars, pipelining, cloud watch",
+        "Creating an SVG images dynamically."
+      ],
+      role: [
+        "Implementated several modules at backend with AWS architecture.",
+        "Team mentoring",
+        "Taking ownership and responsibility for the project and the code.",
+        "Involved in requirement gathering and analysis and deciding implementation."
+      ]
+    },
+    thumbnail: "../../../Assets/Images/flower-svgrepo-com.svg"
+  },
+  {
+    title: "WebSenor Technologies, Fullstack Developer",
+    span: "02/15 – 07/17",
+    role: "requirement gathering, designing and Implementation.",
+    projects: "Nova Farma",
+    info: {
+      tech: ["PHP", "MySQL, HTML, CSS, Javascript"],
+      role: [
+        "Routing mechanism",
+        "Communicating with stakeholders, requirement gathering, and designing.",
+        "Implemented complex medicine warehousing logic"
+      ]
+    },
+    thumbnail: "../../Assets/Images/dinosaur-svgrepo-com.svg"
+  },
+  {
+    title: "Infosys Ltd, Trainee",
+    span: "09/14 – 12/14",
+    role: "Learning Microsoft .NET Technologies",
+    projects: "Shopping portal",
+    info: {
+      tech: ["ASP.NET, ADO.NET", "Java Script, AJAX, HTML, CSS", "MS SQL"],
+      role: [
+        "It is a shopping website using which we can manage admin, user accounts.",
+        "Add/remove products.",
+        "buy products through various WCF and WEB Services, stored procedures, functions."
+      ]
+    },
+    thumbnail: "../../Assets/Images/flower-green.svg"
+  }
+];
