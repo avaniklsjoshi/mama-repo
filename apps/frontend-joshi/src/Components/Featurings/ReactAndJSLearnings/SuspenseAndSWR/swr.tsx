6f40b8bb24d3ffee3304fbb332654f33a35232d9
Avani Joshi <avaniklsjoshi@gmail.com>
2023-02-07 16:39:59 +0100

    frontend-joshi: interview prep swr

--- apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/SuspenseAndSWR/swr.tsx
+++ apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/SuspenseAndSWR/swr.tsx
@@ -1,3 +1,18 @@
+import axios from "axios";
+import useSWR from "swr";
+
 export default function SWR() {
-  return <div>Transition!!!!</div>;
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR(
+    "https://random-facts2.p.rapidapi.com/getfact",
+    fetcher
+  );
+  if (error) return <div>Request Failed</div>;
+  if (!data) return <div>Loading...</div>;
+  return (
+    <div>
+      <h1>Random Fact:</h1>
+      <p>{data.randomFact}</p>
+    </div>
+  );
 }
