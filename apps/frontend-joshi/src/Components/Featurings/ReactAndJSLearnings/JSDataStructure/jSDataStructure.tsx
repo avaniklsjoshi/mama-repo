5eaa335b661a6bef9114f8fb2778b32151d64f28
Avani Joshi <avaniklsjoshi@gmail.com>
2023-03-27 13:39:31 +0200

    frontend-joshi: Stack

--- apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/jSDataStructure.tsx
+++ apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/jSDataStructure.tsx
@@ -1,11 +1,13 @@
 import ArrayAndSets from "./arrayAndSets";
 import ObjectAndMaps from "./objectAndMaps";
+import Stacks from "./stack";
 
 export default function DesignPatterns() {
   return (
     <div>
       <ArrayAndSets />
       <ObjectAndMaps />
+      <Stacks />
     </div>
   );
 }
