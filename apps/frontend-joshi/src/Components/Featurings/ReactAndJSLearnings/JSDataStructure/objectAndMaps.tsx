0fd11c80fb1c15b05fd851e9f531a304576af7e5
Avani Joshi <avaniklsjoshi@gmail.com>
2023-03-27 11:02:38 +0200

    frontend-joshi: Objects and maps

--- apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/objectAndMaps.tsx
+++ apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/objectAndMaps.tsx
@@ -1,8 +1,24 @@
+// 1. Objects are unordered but maps are ordered
+// 2. Keys in object can only be of type symbol or string, but in maps it can be of any type
+// 3. An Object has a prototype and may contain default keys which may collide with our own keys, but not in maps
+// 4. Object are not iterables, but maps are
+// 5. Length of the object needs to be determined manually, but it is readily available in maps
+// 6. we can attach functionality to objects but maps can only store data
+
 export default function ObjectAndMaps() {
-  return (
-    <div>
-      Object And Maps
-      <div>sdsdsd</div>
-    </div>
-  );
+  const maps = new Map([
+    ["a", 1],
+    ["b", 2]
+  ]);
+  maps.set("c", 3); // add value
+  maps.delete("b"); // delete value
+  maps.set("c", 3); // add value
+  console.log(`Maps size: ${maps.size}`);
+  console.log(`Maps contains key: ${maps.has("a")}`);
+  maps.clear(); // delete all in maps
+
+  for (const [key, value] of maps) {
+    console.log(`Maps: ${key}: ${value}`);
+  }
+  return <div>Object And Maps</div>;
 }
