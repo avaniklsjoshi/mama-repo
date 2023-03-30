beb207b79aa3ec45533cfb3b5cee47420842cde0
Avani Joshi <avaniklsjoshi@gmail.com>
2023-03-30 13:02:57 +0200

    frontend-joshi: Queue optimised

--- apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/queue.tsx
+++ apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/queue.tsx
@@ -35,6 +35,50 @@ console.log("que elements:", que.print());
 que.dequeue();
 console.log("que elements after pop:", que.print());
 
+class QueOptimised {
+  items: any;
+  front: number;
+  rear: number;
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+  enqueue(element: number) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+  dequeue() {
+    const firstItem = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return firstItem;
+  }
+  size() {
+    return this.rear - this.front;
+  }
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+  peek() {
+    return this.items[this.front];
+  }
+  print() {
+    console.log(this.items);
+  }
+}
+const queOptimised = new QueOptimised();
+console.log("Is queOptimised Empty:", queOptimised.isEmpty());
+
+queOptimised.enqueue(10);
+queOptimised.enqueue(20);
+queOptimised.enqueue(15);
+console.log("queOptimised size:", queOptimised.size());
+console.log("queOptimised top element:", queOptimised.peek());
+console.log("queOptimised elements:", queOptimised.print());
+queOptimised.dequeue();
+console.log("queOptimised elements after pop:", queOptimised.print());
+
 export default function Queue() {
   return <div>Queue</div>;
 }
