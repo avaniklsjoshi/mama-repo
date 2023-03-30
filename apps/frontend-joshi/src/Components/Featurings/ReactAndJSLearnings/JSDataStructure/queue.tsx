6327e3090cf40ef6f0186712777922b7c84459b5
Avani Joshi <avaniklsjoshi@gmail.com>
2023-03-30 12:43:23 +0200

    frontend-joshi: Queue

--- apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/queue.tsx
+++ apps/frontend-joshi/src/Components/Featurings/ReactAndJSLearnings/JSDataStructure/queue.tsx
@@ -3,11 +3,11 @@ class Que {
   constructor() {
     this.items = [];
   }
-  push(element: number) {
+  enqueue(element: number) {
     return this.items.push(element);
   }
-  pop() {
-    return this.items.pop();
+  dequeue() {
+    return this.items.shift();
   }
   size() {
     return this.items.length;
@@ -16,23 +16,24 @@ class Que {
     return this.items.length === 0;
   }
   peek() {
-    return this.items[this.items.length - 1];
+    if (!this.isEmpty()) return this.items[0];
+    return null;
   }
   print() {
     console.log(this.items.toString());
   }
 }
-const stack = new Que();
-console.log("Is stack Empty:", stack.isEmpty());
+const que = new Que();
+console.log("Is que Empty:", que.isEmpty());
 
-stack.push(10);
-stack.push(20);
-stack.push(15);
-console.log("stack size:", stack.size());
-console.log("stack top element:", stack.peek());
-console.log("stack elements:", stack.print());
-stack.pop();
-console.log("stack elements after pop:", stack.print());
+que.enqueue(10);
+que.enqueue(20);
+que.enqueue(15);
+console.log("que size:", que.size());
+console.log("que top element:", que.peek());
+console.log("que elements:", que.print());
+que.dequeue();
+console.log("que elements after pop:", que.print());
 
 export default function Queue() {
   return <div>Queue</div>;
